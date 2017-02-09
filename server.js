8d08ad1128cf28118837335052a9e5d2392f1b3a
CarrieShort <carrie.r.phillips@gmail.com>
2017-02-08 21:07:21 -0800

    add 404 response for bad routes closes #1

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5050;
 
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
+
 module.exports = app.listen(port, () => console.log('Server up on port: ' + port));
